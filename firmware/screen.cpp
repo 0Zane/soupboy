742c6a9425b5aaf272c9304ceb463a531e342a96
Zane <labs.zane@gmail.com>
2026-07-06 11:54:17 +0800

    Adding screen instructions

--- firmware/screen.cpp
+++ firmware/screen.cpp
@@ -1,2 +1,33 @@
+//PAGE 0 LOADING SCREEN
+// You can just write "Soup boy loading"
 void bootscreen() {}
 
+//MAKE THE NAVBAR ON EVERY PAGE
+
+/*Navigation will be:
+left and right button to go left and right
+middle button to select the menu 
+once in menu, left and right to navigate on the buttons (exemple, in rf: ble jam, copy, emit)
+
+/*PAGE 1 MENU:
+soup avatar at the center
+arrow pointing at it ans wors "soup" linked to the arrow
+somewhere you write "world ending on july 7th"
+add cool futuristic visuals if you can */
+void drawmenu(){}
+
+/*Page 2 TOOLS
+Here we will see the air temperature
+air humidityz
+air pressure
+*/
+
+/*Page 3 DEVICE
+
+*/
+
+
+/*PAGE 4 RF
+Here we have dif
+
+*/
